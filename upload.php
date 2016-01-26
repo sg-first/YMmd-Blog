e30f0c9ee8e4896aa0aab612bf25b30a6a0e9583
Fydui <gtx980sli@yahoo.com>
2016-01-26 23:06:10 +0800

    上传页面撸好了,我也不知道这么整对不对,不对再改,index是页面,还有一个文件夹是图片----果冻

--- upload.php
+++ upload.php
@@ -1,7 +1,42 @@
-<?
-require_once dirname(__FILE__).'/help.php'
+  <?php
+if ((($_FILES["file"]["type"] == "image/gif")
+|| ($_FILES["file"]["type"] == "image/jpeg")
+|| ($_FILES["file"]["type"] == "image/pjpeg"))
+|| ($_FILES["file"]["type"] == "text/plain"))
+&& ($_FILES["file"]["size"] < 10485760))
+  {
+  if ($_FILES["file"]["error"] > 0)
+    {
+    echo "Return Code: " . $_FILES["file"]["error"] . "<br />";
+    }
+  else
+    {
+    echo "Upload: " . $_FILES["file"]["name"] . "<br />";
+    echo "Type: " . $_FILES["file"]["type"] . "<br />";
+    echo "Size: " . ($_FILES["file"]["size"] / 1024) . " Kb<br />";
+    echo "Temp file: " . $_FILES["file"]["tmp_name"] . "<br />";
+
+    if (file_exists("upload/" . $_FILES["file"]["name"]))
+      {
+      echo $_FILES["file"]["name"] . " already exists. ";
+      }
+    else
+      {
+      move_uploaded_file($_FILES["file"]["tmp_name"],
+      "upload/" . $_FILES["file"]["name"]);
+      echo "Stored in: " . "upload/" . $_FILES["file"]["name"];
+      }
+    }
+  }
+else
+  {
+  echo "Invalid file";
+  }
+
+  require_once dirname(__FILE__).'/help.php'
 
 CheckLogin();
 //echo一个上传界面让用户传，上传完毕执行下面的
 //获取目前时间，与上传文件名一同写入time.ini
 //询问用户是否立即更新博客，如果是跳转到generate.php
+?>
