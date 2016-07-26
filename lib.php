b44a71666ba6c18dcccda69a1164440823ecac66
3A <524019351@qq.com>
2016-07-26 17:27:04 +0800

    删除一些毫无卵用的东西，准备做最后检查

--- lib.php
+++ lib.php
@@ -3,24 +3,7 @@ require_once dirname(__FILE__) . '/mdlib/Markdown.php';
 require_once dirname(__FILE__) . '/mdlib/MarkdownExtra.php';
 header("Content-Type: text/html; charset=utf-8");
 
-$explain='ExplainMarkdown';//超链接接口默认调用的解释器是Markdown
-$instruction = explode(",",$_POST["instruction"]);
-if($instruction[0]=="WriteFile")
-{
-    if($instruction[3]==null)
-    {WriteFile($instruction[1],$instruction[2]);}
-    else
-    {WriteFile($instruction[1],$instruction[2],$instruction[3]);}
-}
-if($instruction[0]=="ExplainAndEcho")
-{
-    if($instruction[2]==null)
-    {ExplainAndEcho($instruction[1]);}
-    else
-    {ExplainAndEcho($instruction[1],$instruction[2]);}
-}
-
-//后台接口函数
+//一层包装
 function ExplainMarkdown($path,$font=null)
 {
 	$text = file_get_contents($path);
@@ -33,7 +16,7 @@ function ExplainMarkdownExtra($path)
 	return Michelf\MarkdownExtra::defaultTransform($text);
 }
 
-//超链接接口函数
+//真正使用的
 function ExplainAndEcho($path,$font=null)
 {
     global $explain;
